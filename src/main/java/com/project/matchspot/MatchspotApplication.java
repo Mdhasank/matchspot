80f2533700465bf24e3a20f920efbd94fdd79c8c
Mohd Hasan Khan <mhasan4368@gmail.com>
2024-04-26 20:21:54 +0530

    Initial commit

---
+++ src/main/java/com/project/matchspot/MatchspotApplication.java
@@ -0,0 +1,13 @@
+package com.project.matchspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchspotApplication.class, args);
+	}
+
+}
